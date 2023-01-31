a6c33f4962960a45c1658baf429a2166e8e1923f
Gabor Zorad EPAM <108816298+gabor-zorad-epam@users.noreply.github.com>
2023-01-31 16:54:29 +0100

    Implement login validation proto_version.

--- main.py
+++ main.py
@@ -7,9 +7,18 @@ def welcome():
         3 : "Apply for a Job"
     }
 
+    usr_accounts = {
+        "boss" : "pass_boss",
+        "team_lead" : "team_lead",
+        "hr" : "pass_hr",
+        "employee" : "pass_emp",
+        "intern": "pass_intern",
+
+    }
     for i in range(1,len(menu) + 1):
         print(f"{i}. {menu[i]}")
-
+        #login_validation(menu, usr_accounts)
+def login_validation(menu, usr_accounts):
     #Inside the input validate if the entered value is bigger/smaller than the length of menu.
     while(True):
         try:
@@ -17,7 +26,20 @@ def welcome():
 
             if(usr_input < 1 or usr_input > len(menu)):
                 raise ValueError
-            break
+
+            if usr_input == 1:
+                usr_name = input("Please enter your username: ")
+
+                if usr_name not in usr_accounts:
+                    print("Invalid username!")
+                else:
+                    usr_pass = input("Please enter your password: ")
+                    if usr_pass != usr_accounts[usr_name]:
+                        print("Invalid password!")
+                    else:
+                        print("Successful login!")
+                        break
+
         except ValueError:
             print("Invalid input! Please enter a correct menu number!")
 
